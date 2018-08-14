9de552e8c1b972e92c67fbac2195a508126081c1
mcintdre000 <38018234+mcintdre000@users.noreply.github.com>
2018-08-13 19:52:18 -0700

    Servers component

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "../../../node_modules/@angular/core";
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+
+export class ServerComponent {
+
+}
